bb283a2bfa4a35b7fe36b67c1f6ac89ac190d904
Tushar Maheshwari <tushar.mahi1304@gmail.com>
2026-03-16 12:56:46 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.use("/auth", authRoutes);
+app.use("/todos", authMiddleware, todoRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server start at http://localhost:${PORT}`);
+});
